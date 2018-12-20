5c359d34cd885d106dc215e3170fba34e6ae7504
rundeploy <cristianmitul@gmail.com>
2018-12-20 15:24:14 +0000

    Exercice 3 udemy

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace NorthWindExercise
+{
+    partial class formNorthWind
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerId = new System.Windows.Forms.TextBox();
+            this.txtCompName = new System.Windows.Forms.TextBox();
+            this.txtContName = new System.Windows.Forms.TextBox();
+            this.txtContTitle = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contact Title";
+            // 
+            // txtCustomerId
+            // 
+            this.txtCustomerId.Location = new System.Drawing.Point(217, 46);
+            this.txtCustomerId.Name = "txtCustomerId";
+            this.txtCustomerId.Size = new System.Drawing.Size(100, 20);
+            this.txtCustomerId.TabIndex = 4;
+
+            // 
+            // txtCompName
+            // 
+            this.txtCompName.Location = new System.Drawing.Point(217, 91);
+            this.txtCompName.Name = "txtCompName";
+            this.txtCompName.Size = new System.Drawing.Size(100, 20);
+            this.txtCompName.TabIndex = 5;
+            // 
+            // txtContName
+            // 
+            this.txtContName.Location = new System.Drawing.Point(217, 147);
+            this.txtContName.Name = "txtContName";
+            this.txtContName.Size = new System.Drawing.Size(100, 20);
+            this.txtContName.TabIndex = 6;
+            // 
+            // txtContTitle
+            // 
+            this.txtContTitle.Location = new System.Drawing.Point(217, 210);
+            this.txtContTitle.Name = "txtContTitle";
+            this.txtContTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtContTitle.TabIndex = 7;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(56, 325);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(75, 23);
+            this.btnFirst.TabIndex = 8;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(174, 325);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 9;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(286, 325);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 10;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(420, 325);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(75, 23);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // formNorthWind
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.txtContTitle);
+            this.Controls.Add(this.txtContName);
+            this.Controls.Add(this.txtCompName);
+            this.Controls.Add(this.txtCustomerId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "formNorthWind";
+            this.Text = "North Wind";
+            this.Load += new System.EventHandler(this.formNorthWind_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerId;
+        private System.Windows.Forms.TextBox txtCompName;
+        private System.Windows.Forms.TextBox txtContName;
+        private System.Windows.Forms.TextBox txtContTitle;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnLast;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace NorthWindExercise
+{
+    public partial class formNorthWind : Form
+    {
+
+        OleDbConnection connection;
+        OleDbCommand commandSelectCustomers;
+        OleDbDataAdapter adapterCustomer;
+        DataTable tableCustomers;
+        CurrencyManager currencyManager;
+
+        public formNorthWind()
+        {
+            InitializeComponent();
+        }
+
+        private void formNorthWind_Load(object sender, EventArgs e)
+        {
+            var connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;
+                Data Source=D:\PROJETOS\UdemyCourse (Alex)\NorthWindExercise UdemyProject3\NorthWind.accdb;
+                Persist Security Info = False; ";
+
+            connection = new OleDbConnection(connectionString);
+            connection.Open();
+
+
+            commandSelectCustomers = new OleDbCommand("SELECT * FROM CUSTOMERS", connection);
+            adapterCustomer = new OleDbDataAdapter();
+            adapterCustomer.SelectCommand = commandSelectCustomers;
+            tableCustomers = new DataTable();
+            adapterCustomer.Fill(tableCustomers);
+
+            //bind controls
+            txtCustomerId.DataBindings.Add("Text", tableCustomers, "CustomerID");
+            txtCompName.DataBindings.Add("Text", tableCustomers, "CompanyName");
+            txtContName.DataBindings.Add("Text", tableCustomers, "ContactName");
+            txtContTitle.DataBindings.Add("Text", tableCustomers, "ContactTitle");
+
+            //establish currency manager
+            currencyManager = (CurrencyManager)BindingContext[tableCustomers];
+
+
+            connection.Close();
+            connection.Dispose();
+            adapterCustomer.Dispose();
+            tableCustomers.Dispose();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            currencyManager.Position = 0;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            currencyManager.Position++;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            currencyManager.Position--;
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            currencyManager.Position = currencyManager.Count;
+        }
+
+
+    }
+}
